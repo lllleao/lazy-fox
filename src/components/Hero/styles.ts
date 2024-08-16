bd24c5e4b0675b436aec8e63ad935f1e7d111ad5
leao <lucasleaolima@gmail.com>
2024-08-16 16:11:17 -0300

    test

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -13,7 +13,7 @@ export const HeroSection = styled.section`
     }
 
     .desc {
-        font-family: Barlow;
+        font-family: 'Barlow';
         font-weight: normal;
 
         font-size: 2rem;
